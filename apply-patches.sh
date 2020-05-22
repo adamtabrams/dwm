531a323109d0691ce77c2393cb6bd3616f7f67e5
Adam Abrams <adam.abrams@optum.com>
2020-05-22 01:37:08 -0500

    add personal config patch

--- apply-patches.sh
+++ apply-patches.sh
@@ -5,6 +5,7 @@ patches="\
     patches/dwm-movestack-6.1.diff
     patches/dwm-vanitygaps-20190508-6.2.diff
     patches/dwm-shiftview-20200522.diff
+    patches/dwm-config-20200522.diff
 "
 
 echo "$patches" | xargs -n 1 git apply --whitespace=fix -3
