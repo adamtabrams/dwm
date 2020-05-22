f854d51a310271bdd2d94d2c44cc1cc0cf7d4fa3
Adam Abrams <adam.abrams@optum.com>
2020-05-22 01:27:38 -0500

    add shiftview patch

--- apply-patches.sh
+++ apply-patches.sh
@@ -4,6 +4,7 @@ patches="\
     patches/dwm-swallow-20170909-ceac8c9.diff
     patches/dwm-movestack-6.1.diff
     patches/dwm-vanitygaps-20190508-6.2.diff
+    patches/dwm-shiftview-20200522.diff
 "
 
 echo "$patches" | xargs -n 1 git apply --whitespace=fix -3
